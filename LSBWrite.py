2789bd3190422bf0857b31dacf41405fd2588bb3
Alex Soong <2236325021@qq.com>
2022-03-25 15:21:24 +0800

    finished color to grey

--- LSBWrite.py
+++ LSBWrite.py
@@ -1,8 +1,15 @@
 from PIL import Image
 import numpy as np
+import sys, os
 
-im = Image.open("123.bmp")
-x = np.array(im)
+if len(sys.argv) > 1:
+    fileName = sys.argv[1]
+else:
+    print("未指定文件名")
+    os._exit(0)
 
-for xi in x:
-    print(xi)
+im0 = Image.open(fileName)
+x = np.array(im0)
+
+im1 = Image.fromarray(x)
+im1.save(fileName[:-4] + "_written.bmp")
--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 
 用到的库：NumPy、PIL
 
+
+
+color2grey.py：将彩色的bmp图像转为灰度图
+
+LSBWrite.py：将二进制信息以LSB方式写入bmp图像
+
