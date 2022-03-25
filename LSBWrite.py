0d0bcb66c495d5c9730ba0fd96862fb7edcf9d0a
Alex Soong <2236325021@qq.com>
2022-03-25 16:21:23 +0800

    start to write LSBWrite

--- LSBWrite.py
+++ LSBWrite.py
@@ -2,14 +2,34 @@ from PIL import Image
 import numpy as np
 import sys, os
 
-if len(sys.argv) > 1:
-    fileName = sys.argv[1]
-else:
-    print("未指定文件名")
-    os._exit(0)
 
-im0 = Image.open(fileName)
-x = np.array(im0)
+def LSBReplace(_value, _bit=0):
+    lsb = _value % 2
+    _value = _value - lsb + _bit
+    return _value
 
-im1 = Image.fromarray(x)
-im1.save(fileName[:-4] + "_written.bmp")
+
+def write(fileName, infotoWrite=""):
+    im0 = Image.open(fileName)
+    x = np.array(im0)
+
+    length = x.ravel().shape[0]
+    for i in range(length):
+        x.ravel()[i] = LSBReplace(x.ravel()[i])
+
+    im1 = Image.fromarray(x)
+    im1.save(fileName[:-4] + "_written.bmp")
+
+
+if __name__ == '__main__':
+
+    # if len(sys.argv) > 2:
+    inputName = sys.argv[1]
+    #     inputName0 = sys.argv[2]
+    # else:
+    #     print("未指定文件名")
+    #     os._exit(0)
+
+    write(inputName)
+
+
